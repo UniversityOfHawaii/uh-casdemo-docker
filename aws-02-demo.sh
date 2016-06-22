22da42f73a8847949cae1547cbfb17363e7b6783
eghm <github-m@eghm.net>
2016-06-22 10:35:37 -1000

    add git pull if directory already exists

--- aws-02-demo.sh
+++ aws-02-demo.sh
@@ -1,5 +1,14 @@
-git clone https://github.com/UniversityOfHawaii/uh-casdemo-docker.git
-cd uh-casdemo-docker/build
+export DEMODIR=uh-casdemo-docker
+
+if [ ! -d "$DEMODIR" ]; then
+    git clone https://github.com/UniversityOfHawaii/uh-casdemo-docker.git
+    cd $DEMODIR/build
+else
+    cd $DEMODIR
+    git pull
+    cd build
+fi
+
 docker build -t em/tomcat .
 # run with /etc/localtime mounted so container runs with the same time zone (Hawaii, see aws-01-docker-install.sh)
 docker run -v /etc/localtime:/etc/localtime:ro -d -p 80:8080 -p 443:8443 em/tomcat
